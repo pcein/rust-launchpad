6c3daddf32fc368c1f6f4d79531dc45c1ae947e4
Pramode C.E <mail@pramode.in>
2016-12-17 20:13:59 +0530

    Remove startup_gcc.d

--- blinky-c/startup_gcc.d
+++
@@ -1,9 +0,0 @@
-startup_gcc.o: startup_gcc.c \
- /home/pramodein/devtools/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h \
- /home/pramodein/devtools/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdint.h \
- /home/pramodein/devtools/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_default_types.h \
- /home/pramodein/devtools/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/features.h \
- /home/pramodein/devtools/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_newlib_version.h \
- /home/pramodein/devtools/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_intsup.h \
- /home/pramodein/devtools/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_stdint.h \
- ../tivaware/inc/hw_nvic.h ../tivaware/inc/hw_types.h
